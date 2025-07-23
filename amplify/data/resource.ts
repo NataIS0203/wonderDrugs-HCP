e1860e1ad89c0f09309c02d7ca82127d7557e76b
Natalya Sniff <natalya.sniff@feedingamerica.org>
2025-07-23 18:21:41 -0500

    installing amplify

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,5 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { hcpRequest } from '../functions/resource';
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -7,11 +8,14 @@ specifies that any unauthenticated user can "create", "read", "update",
 and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
+  hcpRequest: a
+    .query()
+    .arguments({
+      name: a.string(),
     })
-    .authorization((allow) => [allow.guest()]),
+    .returns(a.string())
+    .authorization(allow => [allow.guest()])
+    .handler(a.handler.function(hcpRequest)),
 });
 
 export type Schema = ClientSchema<typeof schema>;
