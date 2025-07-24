fc3a31fbfd0ba55e669e5b6d7d736078f3d24747
Natalya Sniff <natalya.sniff@feedingamerica.org>
2025-07-23 21:00:11 -0500

    installing amplify

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/WDHCP.jpg'
 import './App.css'
 
 function App() {
-   const [name, setName] = useState('');
-   const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
 
   const handleNameChange = (event: { target: { value: SetStateAction<string>; }; }) => {
     setName(event.target.value);
+  };
+
+  const handleEmailChange = (event: { target: { value: SetStateAction<string>; }; }) => {
     setEmail(event.target.value);
   };
 
@@ -15,26 +18,30 @@ function App() {
   return (
     <>
       <div>
-          <img src={reactLogo} className="logo" alt="HCP logo" />
+        <img src={reactLogo} className="logo" alt="HCP logo" />
       </div>
       <h1>Wonder Drugs HCP</h1>
       <div>
-        <label htmlFor="nameInput">HCP Name: </label>
-        <input
-          type="text"
-          id="nameInput"
-          value={name}
-          onChange={handleNameChange}
-        />
-        <label htmlFor="nameInput">HCP Name: </label>
-        <input
-          type="text"
-          id="emailInput"
-          value={email}
-          onChange={handleNameChange}
-        />
-      <p>Hello, {name}!</p>
-       </div>
+        <p>
+          <label htmlFor="nameInput">HCP Name: </label>
+          <input
+            type="text"
+            id="nameInput"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </p>
+        <p>
+          <label htmlFor="nameInput">HCP email: </label>
+          <input
+            type="text"
+            id="emailInput"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </p>
+        <p>Hello, {name}!</p>
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
