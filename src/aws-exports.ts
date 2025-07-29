bf5a124c1460632e7853e04533aa64665ad3bdcb
Natalya Sniff <natalya.sniff@feedingamerica.org>
2025-07-29 18:30:09 -0500

    installing amplify

---
+++ src/aws-exports.ts
@@ -0,0 +1,11 @@
+
+// aws-exports.ts
+const awsconfig = {
+  aws_project_region: "us-east-2",
+  aws_cognito_region: "us-east-2",
+  aws_user_pools_id: "us-east-2_VBepwawaC",
+  aws_user_pools_web_client_id: "auuh9hg8kqj9411lt5mdil3p6",
+  // ... other config values
+};
+
+export default awsconfig;
