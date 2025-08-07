daf9acbe38db6f70256ddea0685ff7cd5ba48d38
Natalya Sniff <natalya.sniff@feedingamerica.org>
2025-08-07 12:39:45 -0500

    added reset

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/WDHCP.jpg'
 import './App.css'
 import type { MSLResponce } from './MSLResponce.js'
 import { fetchHCPData, fetchHCPRequestData } from './getPost.js'
-import { download} from './downloadFile.js'
+import { download} from './downloadFile.ts'
 
 function App() {
   const [name, setName] = useState('Natalya Sniff');
